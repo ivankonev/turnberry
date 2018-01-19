02f813e1b91714da83e8e365d189aa57b377a009
Ivan Konev <hello@ivankonev.com>
2018-01-19 14:37:29 +0300

    dark header

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function(){
 
-	// --- COLLAPSE NAVBAR --- //
+	// --- NAVBAR --- //
 	(function navbarCollapse() {	
 		var nbButton = jQuery('button.navbar-toggle');
 		var nbCollapse = jQuery('div.navbar-collapse');
@@ -13,7 +13,47 @@ jQuery(document).ready(function(){
 				nbCollapse.removeAttr('style');
 			}
 		});
-	})();	
+	})();
 	
+	// --- TOGGLE --- //
+	(function toggleText() {
+		var toggle = jQuery('a.readmore');
+		toggle.on('click', function(e) {
+			e.preventDefault();
+			var toggle = jQuery(this);
+			var toggleText = toggle.text() == "Read More" ? "Show Less" : "Read More";
+			toggle.text(toggleText);
+		});
+	})();
+
+	// --- PARALLAX --- //
+	(function parallaxBackground() {
+  
+    var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+    if ( (!isMobile) && ($('.parallax').length) ) {
+
+      var parallaxContainer = $(".parallax-container"),
+          parallaxImg = $(".parallax-img");
+
+        $(window).on('load resize scroll',function(e) {
+
+          var windowHeight = $(window).height(),
+          scroll = $(window).scrollTop();
+          function parallax(parallaxContainer, parallaxImg, speed) {
+
+            var offsetTop = parallaxContainer.offset().top,
+            height = parallaxContainer.height();
+            parallaxImg.filter(function() {
+            return offsetTop < ( scroll + windowHeight ) && ( offsetTop + height ) > scroll;
+          }).css( 'transform', 'translate3d( 0px, ' + parseInt( ( scroll - offsetTop ) * speed ) + 'px, 0px )' );
+
+        }
+          
+        parallax( parallaxContainer, parallaxImg, 0.2 );
+          
+      });
+    }
+  })();
 });
 
